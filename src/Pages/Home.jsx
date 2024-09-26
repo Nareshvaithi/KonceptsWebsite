cda3baa7bacfa29705987753b0c140f3560be746
SanthoshMern <santhosh.sugavaneshwaran@gmail.com>
2024-09-26 13:16:07 +0530

    second commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,8 @@
-
 import { BackgroundAnimation } from "hero-slider/dist/components/Slide/Background";
 import FaqAccordian from "../Components/FAQ/Accordian";
 import BackgroundSlider from "../Components/BackgroundSlider/BackgroundSlider";
 import PriceDetails from "../Components/PriceDetails/PriceDetails";
+
 const Home = ()=>{
  
     return(
